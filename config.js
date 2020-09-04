089da88cc0b96fe01cd4d06d1791d78d097d0fe6
Ulysses Kuntze <kuntze@gmail.com>
2020-09-04 18:23:01 +1200

    build(setup): Setup project to work with AWS

---
+++ config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ap-southeast-2",
+    BUCKET: "notes-app-uploads-7834",
+  },
+  apiGateway: {
+    REGION: "ap-southeast-2",
+    URL: "https://gcrfyf3bb3.execute-api.ap-southeast-2.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "ap-southeast-2",
+    USER_POOL_ID: "ap-southeast-2_IQUKecmOB",
+    APP_CLIENT_ID: "2g7e01ejjjv0pjnmqdadhq8ujr",
+    IDENTITY_POOL_ID: "ap-southeast-2:6506fa81-5a93-4b03-beef-c6165ea3d446",
+  },
+};
